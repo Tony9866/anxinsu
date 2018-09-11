7b372eccd719b0fb4744b021b550dbd15ca67b9b
littlevegbe <tonyzhenzhang@hotmail.com>
2018-09-11 15:54:12 +0800

    头部轮播改动，新增一个分支

--- LigerRM.WebSite/AppUpSetJS/AppHome.js
+++ LigerRM.WebSite/AppUpSetJS/AppHome.js
@@ -4,11 +4,11 @@
 var rootPath = "../";
 //列表结构
 var grid = $("#maingrid").ligerGrid({
-    columns: [{ display: "编号", name: "BannerId", width: 100, type: "text", align: "left" },
-                { display: "类型", name: "BannerType", width: 200, type: "textarea", align: "left" },
-                { display: "图片地址", name: "ImageUrl", width: 50, type: "text", align: "left" },
-                { display: "地址", name: "Url", width: 150, type: "text", align: "left" },
-                { display: "描述", name: "Describe", width: 150, type: "text", align: "left"}],
+    columns: [{ display: "编号", name: "BannerId", width: 50, type: "text", align: "left" },
+                { display: "类型", name: "BannerType", width: 50, type: "textarea", align: "left" },
+                { display: "图片地址", name: "ImageUrl", width: 200, type: "text", align: "left" },
+                { display: "地址", name: "Url", width: 200, type: "text", align: "left" },
+                { display: "描述", name: "Describe", width: 300, type: "text", align: "left"}],
    
     dataAction: 'server', pageSize: 3, toolbar: {},
     url: rootPath + 'handler/grid.ashx?view=AppHome_Banner', sortName: 'BannerId',
@@ -20,9 +20,6 @@ var grid = $("#maingrid").ligerGrid({
 LG.setGridDoubleClick(grid, 'modify');
 
 
-
-
-
 //加载toolbar
 LG.loadToolbar(grid, toolbarBtnItemClick);
 
@@ -31,18 +28,21 @@ function toolbarBtnItemClick(item) {
     switch (item.id) {
         case "add":
             //top.f_addTab(null, '增加客户信息', 'CustomerManage/CustomersDetail.aspx');
-            wopen('AppHomeEdit.aspx?BannerId=0', '新增房源', '1200', '870');
+            wopen('AppHomeEdit.aspx?BannerId=0', '新增头部', '1200', '870');
             break;
         case "view":
             var selected = grid.getSelected();
+            if (!selected) { LG.tip('请选择行!'); return } 
             wopen('AppHomeEdit.aspx?BannerId=' + selected.BannerId, '编辑APP首页轮播图', '1200', '870');
             break;
         case "modify":
             var selected = grid.getSelected();
+            if (!selected) { LG.tip('请选择行!'); return } 
             wopen('AppHomeEdit.aspx?BannerId=' + selected.BannerId, '编辑APP首页轮播图', '1200', '870');
             break;
         case "delete":
             var selected = grid.getSelected();
+            if(selected){
             jQuery.ligerDialog.confirm('确定删除吗?', function (confirm) {
                 if (confirm)
                 $.ajax({
@@ -62,32 +62,36 @@ function toolbarBtnItemClick(item) {
                     }
                 });
             });
+          }else
+           {
+            LG.tip('请选择行!');
+          }
             break;
         //case "viewRentPerson": 
         //    var selected = grid.getSelected(); 
         //    if (!selected) { LG.tip('请选择行!'); return } 
         //    $.ligerDialog.open({ height: 450, width: 750, url: 'RentAttribute.aspx?RID=' + selected.RentNO, title: '承租人信息' }); 
         //    break; 
-        case "viewHistory":
-            var selected = grid.getSelected();
-            if (!selected) { LG.tip('请选择行!'); return }
-            $.ligerDialog.open({ height: 450, width: 850, url: 'RentInfoHistory.aspx?RentNo=' + selected.RentNO + '&type=V', title: '租赁历史信息' });
-            break;
-        case "export":
-            $.ligerDialog.open({ url: "../print.aspx?exporttype=xls" }); return;
-            break;
-        case "image":
-            var rows = grid.getCheckedRows();
-            var signetIds = '';
-            for (var i = 0, l = rows.length; i < l; i++) {
-                signetIds += ",'" + rows[i].se_signet_id + "'";
+//        case "viewHistory":
+//            var selected = grid.getSelected();
+//            if (!selected) { LG.tip('请选择行!'); return }
+//            $.ligerDialog.open({ height: 450, width: 850, url: 'RentInfoHistory.aspx?RentNo=' + selected.RentNO + '&type=V', title: '租赁历史信息' });
+//            break;
+//        case "export":
+//            $.ligerDialog.open({ url: "../print.aspx?exporttype=xls" }); return;
+//            break;
+//        case "image":
+//            var rows = grid.getCheckedRows();
+//            var signetIds = '';
+//            for (var i = 0, l = rows.length; i < l; i++) {
+//                signetIds += ",'" + rows[i].se_signet_id + "'";
 
-            }
-            var selected = grid.getSelected();
-            if (!selected) { LG.tip('请选择行!'); return; }
+//            }
+//            var selected = grid.getSelected();
+//            if (!selected) { LG.tip('请选择行!'); return; }
 
-            top.f_addTab(null, selected.RentNO + '-房源照片', '../RentHouse/RentImageList.aspx?RentNO=' + selected.RentNO);
-            break;
+//            top.f_addTab(null, selected.RentNO + '-房源照片', '../RentHouse/RentImageList.aspx?RentNO=' + selected.RentNO);
+//            break;
     }
 }
 
@@ -101,24 +105,24 @@ function wopen(pageURL, title, w, h) {
     var random = Math.floor(Math.random() * (1000 + 1));
     var targetWin = window.open(pageURL + '&' + random, title, ' width=' + w + ', height=' + h + ', top=' + top + ', left=' + left + ',scrollbars=yes');
 }
-function f_delete() {
-    var selected = grid.getSelected();
-    if (selected) {
-        LG.ajax({
-            type: 'AjaxBaseManage',
-            method: 'RemoveRent',
-            loading: '正在删除中...',
-            data: { ID: selected.RentNO },
-            success: function () {
-                LG.showSuccess('删除成功');
-                f_reload();
-            },
-            error: function (message) {
-                LG.showError(message);
-            }
-        });
-    }
-    else {
-        LG.tip('请选择行!');
-    }
-}
+//function f_delete() {
+//    var selected = grid.getSelected();
+//    if (selected) {
+//        LG.ajax({
+//            type: 'AjaxBaseManage',
+//            method: 'RemoveRent',
+//            loading: '正在删除中...',
+//            data: { ID: selected.RentNO },
+//            success: function () {
+//                LG.showSuccess('删除成功');
+//                f_reload();
+//            },
+//            error: function (message) {
+//                LG.showError(message);
+//            }
+//        });
+//    }
+//    else {
+//        LG.tip('请选择行!');
+//    }
+//}
